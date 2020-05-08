0e7b319979b1a531fc0bc79faeb2a61418e9929e
Shvejan <49836470+Shvejan@users.noreply.github.com>
2020-05-08 21:25:24 +0530

    location api

--- src/components/RecievedStores.js
+++ src/components/RecievedStores.js
@@ -4,23 +4,27 @@ import "./css/recievedStores.css";
 import { Card } from "react-bootstrap";
 
 const StoresCard = (props) => {
-  return (
-    <div className="justify-content-center">
-      <Card className="storeCard">
-        <div className="row justify-content-center">
-          <div className="col-5 align-items-center">
-            <img src={props.store.logo} className="storeImage" />
+  if (props.store.product_count !== "0") {
+    return (
+      <div className="justify-content-center">
+        <Card className="storeCard">
+          <div className="row justify-content-center">
+            <div className="col-5 align-items-center">
+              <img src={props.store.logo} className="storeImage" />
+            </div>
+            <div className="col">
+              <h6>{props.store.branch_name}</h6>
+              <p>Phone: {props.store.phone}</p>
+              <p>{props.store.operation_time}</p>
+              <p>Total avaliable products {props.store.product_count}</p>
+            </div>
           </div>
-          <div className="col">
-            <h6>{props.store.branch_name}</h6>
-            <p>Phone: {props.store.phone}</p>
-            <p>{props.store.operation_time}</p>
-            <p>Total avaliable products {props.store.product_count}</p>
-          </div>
-        </div>
-      </Card>
-    </div>
-  );
+        </Card>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 };
 
 const RecievedStores = (props) => {
