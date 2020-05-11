b12e6e3adf0a954675c7aad700d9d49bfe9bbc77
Shvejan <49836470+Shvejan@users.noreply.github.com>
2020-05-11 11:30:14 +0530

    quantity added

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,7 +3,14 @@ import { connect } from "react-redux";
 import { fetchProducts, productsClear } from "../redux/ActionCreators";
 import { Loading } from "./Loading";
 import { Redirect } from "react-router-dom";
-import { Card, CardBody } from "reactstrap";
+import {
+  Card,
+  CardBody,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Label,
+} from "reactstrap";
 const mapStateToProps = (state) => ({
   products: state.products,
 });
@@ -41,46 +48,182 @@ class Products extends Component {
     }
   }
 }
-const ProductCard = (props) => {
-  return (
-    <div className="col-sm-3" style={{ "margin-bottom": "50px" }}>
-      <div>
-        <Card style={{ height: "18rem", width: "13rem" }}>
-          <div
+class ProductCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+      quantity: 0,
+    };
+  }
+  toggleModal = () => {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  };
+  handleAdd = () => {
+    if (this.state.quantity < this.props.p.quantity) {
+      this.setState({ quantity: this.state.quantity + 1 });
+    }
+  };
+  handleSub = () => {
+    this.setState({ quantity: this.state.quantity - 1 });
+  };
+  buttons = () => {
+    if (this.state.quantity === 0) {
+      return (
+        <button
+          onClick={this.handleAdd}
+          style={{
+            "background-color": "#41ada4",
+            width: "80px",
+            height: "30px",
+            color: "white",
+            border: "0px",
+            "border-radius": "5px",
+          }}
+        >
+          ADD
+        </button>
+      );
+    } else {
+      return (
+        <div>
+          <button
+            onClick={this.handleSub}
             style={{
-              height: "200px",
-              width: "180px",
-              "text-align": "center",
+              "background-color": "#41ada4",
+              width: "30px",
+              height: "30px",
+              color: "white",
+              border: "0px",
+              "border-radius": "5px",
             }}
           >
-            <img
-              top
-              src={props.p.image}
-              alt="Card image cap"
-              className="hoverImages"
-            />
-          </div>
-
-          <CardBody>
-            <h6 className="cardText" style={{ height: "30px" }}>
-              {props.p.product_name}
-            </h6>
-            <h6
-              className="cardText"
-              style={{ color: "#7fd638", height: "30px" }}
+            -
+          </button>
+          <button
+            style={{
+              "background-color": "#fff",
+              width: "30px",
+              height: "30px",
+              color: "black",
+              border: "0px",
+              "border-radius": "5px",
+            }}
+          >
+            {this.state.quantity}
+          </button>
+          <button
+            onClick={this.handleAdd}
+            style={{
+              "background-color": "#41ada4",
+              width: "30px",
+              height: "30px",
+              color: "white",
+              border: "0px",
+              "border-radius": "5px",
+            }}
+          >
+            +
+          </button>
+        </div>
+      );
+    }
+  };
+  render() {
+    return (
+      <div className="col-sm-3" style={{ "margin-bottom": "50px" }}>
+        <div>
+          <Card
+            style={{ height: "18rem", width: "13rem" }}
+            onClick={this.toggleModal}
+          >
+            <div
+              style={{
+                height: "200px",
+                width: "180px",
+                "text-align": "center",
+              }}
             >
-              <span
-                class="fas fa-rupee-sign"
-                style={{ "margin-right": "5px" }}
-              ></span>
-              {props.p.mrp}
+              <img
+                top
+                src={this.props.p.image}
+                alt="Card image cap"
+                className="hoverImages"
+              />
+            </div>
+
+            <CardBody>
+              <h6 className="cardText" style={{ height: "30px" }}>
+                {this.props.p.product_name}
+              </h6>
+              <h6
+                className="cardText"
+                style={{ color: "#7fd638", height: "30px" }}
+              >
+                <span
+                  class="fas fa-rupee-sign"
+                  style={{ marginRight: "5px" }}
+                ></span>
+                {this.props.p.selling_price}
+              </h6>
+            </CardBody>
+          </Card>
+          <div style={{ marginLeft: "60px" }}>{this.buttons()}</div>
+        </div>
+        <Modal
+          isOpen={this.state.isModalOpen}
+          toggle={this.toggleModal}
+          scrollable={true}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <ModalHeader toggle={this.toggleModal}>Product details</ModalHeader>
+          <ModalBody>
+            <div className="row">
+              <div className="col-3">
+                <img
+                  height="150px"
+                  src={this.props.p.image}
+                  alt="Card image cap"
+                ></img>
+              </div>
+              <div className="col offset-1">
+                <h5>{this.props.p.product_name}</h5>
+                <h6
+                  className="cardText"
+                  style={{ color: "#e78536", height: "30px" }}
+                >
+                  <span
+                    class="fa fa-percent"
+                    style={{ marginRight: "5px" }}
+                  ></span>
+                  {this.props.p.selling_price}
+                </h6>
+                {this.buttons()}
+              </div>
+            </div>
+            <hr />
+            <h6 style={{ fontWeight: "bold", color: "#41ada4" }}>
+              Product Description
             </h6>
-          </CardBody>
-        </Card>
-        <button className="sub-product-btn">ADD</button>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum
+            </p>
+          </ModalBody>
+        </Modal>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Products);
