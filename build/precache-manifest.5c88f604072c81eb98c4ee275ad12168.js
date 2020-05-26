4108ca1fcccccc1ee47b063d1179a58092200276
Shvejan <49836470+Shvejan@users.noreply.github.com>
2020-05-26 17:31:30 +0530

    nearbygrocer.com build

---
+++ build/precache-manifest.5c88f604072c81eb98c4ee275ad12168.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1455b09c3463be9f3130ed97ccbbc6d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcd34b3493529ff54569",
+    "url": "/static/css/2.f0e045c5.chunk.css"
+  },
+  {
+    "revision": "40812d24e9937dd1628e",
+    "url": "/static/css/main.97b6f9d2.chunk.css"
+  },
+  {
+    "revision": "fcd34b3493529ff54569",
+    "url": "/static/js/2.00c2bf9b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.00c2bf9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40812d24e9937dd1628e",
+    "url": "/static/js/main.7438beef.chunk.js"
+  },
+  {
+    "revision": "e8064337d68ededd87a8",
+    "url": "/static/js/runtime-main.86436344.js"
+  },
+  {
+    "revision": "ad01cf9ab53bae0dc9496c81fc8e8413",
+    "url": "/static/media/avatar.ad01cf9a.png"
+  },
+  {
+    "revision": "35344cc40bd0c3eb2f3837c860420e22",
+    "url": "/static/media/cart.35344cc4.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "cfc4da7810da241e0e6b2d8f3772ef84",
+    "url": "/static/media/location.cfc4da78.png"
+  },
+  {
+    "revision": "fe9d96663788aa82701e13b3b44a8d40",
+    "url": "/static/media/logo-big.fe9d9666.png"
+  },
+  {
+    "revision": "ac8c999e083778c0d71b68168825f77a",
+    "url": "/static/media/only-background.ac8c999e.png"
+  },
+  {
+    "revision": "31cffbd600aa5eecfbd1ea82c41f1743",
+    "url": "/static/media/watermark.31cffbd6.png"
+  }
+]);
